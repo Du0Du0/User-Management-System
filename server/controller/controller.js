4d02236ee6cf503dbc35a7f2cac44bae2a2c29ec
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 <dohyun911@hanmail.net>
2023-04-27 17:52:31 +0900

    postman create find update delete api test done

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -65,5 +65,21 @@ exports.update = (req,res) => {
 
 //Delete a user with specified user id in the request
 exports.delete = (req,res) => {
-    
+    const id = req.params.id;
+
+    Userdb.findByIdAndDelete(id)
+    .then(data => {
+        if (!data){
+            res.status(404).send ({message: `Cannot Delete with ${id}. Maybe id is wrong`})
+        } else {
+            res.send ({
+                message : "User was deleted successfully"
+            })
+        }
+    })
+    .catch(err =>{
+        res.status(500).send({
+            message: "Could not delete User with id= " +id
+        })
+    });
 }
