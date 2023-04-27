32599a352ca6bfc3f18cf66e03b2d495a3939d86
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 <dohyun911@hanmail.net>
2023-04-27 17:31:27 +0900

    postman create find update api 테스트 완료

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -33,15 +33,37 @@ exports.create = (req,res) =>{
 
 //retrieve and return all users/ retrieve and return a single user
 exports.find = (req,res) => {
-    
+    Userdb.find()
+    .then(user=> {
+        res.send(user)
+    })
+    .catch(err =>{
+        res.status(500).send({ message : err.message || "Error Occured while retrieving user information"})
+    })
 }
 
 //Update a new identified usr by user id
 exports.update = (req,res) => {
-
+    if(!req.body) {
+        return res
+        .status(400)
+        .send({message: "Data to update can not be empty"})
+    }
+    const id = req.params.id;
+    Userdb.findByIdAndUpdate(id, req.body,{useFindAndModify: false})
+    .then(data => {
+        if(!data){
+            res.status(404).send({message : `Cannot Update user with ${id}. Maybe user not found`})
+        } else {
+            res.send(data)
+        }
+    })
+    .catch(err => {
+        res.status(500).send({ message : "Error Update user information"})
+    })
 }
 
 //Delete a user with specified user id in the request
 exports.delete = (req,res) => {
-
+    
 }
