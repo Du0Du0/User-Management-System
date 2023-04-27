627c41fe9a99b94cfb8380f26cfb525c3e4a2c20
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 <dohyun911@hanmail.net>
2023-04-27 14:31:00 +0900

    Userdb API

---
+++ server/controller/controller.js
@@ -0,0 +1,21 @@
+var Userdb = require('../model/model');
+
+//create and save new user
+exports.create = (req,res) =>{
+    
+}
+
+//retrieve and return all users/ retrieve and return a single user
+exports.find = (req,res) => {
+
+}
+
+//Update a new identified usr by user id
+exports.update = (req,res) => {
+
+}
+
+//Delete a user with specified user id in the request
+exports.delete = (req,res) => {
+
+}
--- server/routes/router.js
+++ server/routes/router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const route = express.Router()
 
 const services = require('../services/render');
+const controller = require('../controller/controller');
 
 /* 
 * @description Root Route
@@ -20,5 +21,13 @@ route.get('/add-user',services.add_user);
 * @method GET /update-user 
 */
 route.get('/update-user',services.update_user);
+
+//API
+route.post('/api/users', controller.create);
+route.get('/api/users', controller.find);
+//Update a new identified usr by user id
+route.put('/api/users/:id', controller.update);
+//Delete a user with specified user id in the request
+route.delete('/api/users/:id', controller.delete);
 
 module.exports = route
